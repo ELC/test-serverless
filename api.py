66559a202e02645f0c3cecdbe12d2ef9e324e527
Ezequiel Leonardo Castaño <elcg@gmx.com>
2020-11-06 12:58:14 +0100

    Remove Uvicorn and add slow endpoint

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from fastapi import FastAPI
 
@@ -13,3 +14,8 @@ async def root():
 async def message():
     message = os.environ.get("message")
     return {"message": message}
+
+@app.get("/slow")
+async def message():
+    time.sleep(10)
+    return {"message": "I take 10 seconds"}
